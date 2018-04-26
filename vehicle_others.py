1b6438e5e5cc0ecfa2a4c56ab2ae4146034f44cc
mahfuz195 <mdmhafi@clemson.edu>
2018-04-26 16:34:46 -0400

    other vehicle data transmission solved

--- vehicle_others.py
+++ vehicle_others.py
@@ -118,7 +118,7 @@ class BroadcastData(threading.Thread):
               cs = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP)
               cs.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
               cs.setsockopt(SOL_SOCKET, SO_BROADCAST, 1)
-              cs.sendto(data,('',4499))
+              cs.sendto(data,('192.168.2.1',4499))
                       
           time.sleep(0.1)
 	  self.steps+=0.1
