40fadc1106d4fa4c7c9983b1edf899a2c087083e
mdr <mdr@freeman76dhcp177.ces.clemson.edu>
2018-04-26 12:29:37 -0400

    forward script added

---
+++ forward_rsu.sh
@@ -0,0 +1,31 @@
+#! /bin/bash
+
+IPTABLES=/sbin/iptables
+
+WANIF='wave-data'
+LANIF='eth0'
+
+# enable ip forwarding in the kernel
+echo 'Enabling Kernel IP forwarding...'
+/bin/echo 1 > /proc/sys/net/ipv4/ip_forward
+
+# flush rules and delete chains
+echo 'Flushing rules and deleting existing chains...'
+#$IPTABLES -F
+#$IPTABLES -X
+
+# enable masquerading to allow LAN internet access
+echo 'Enabling IP Masquerading and other rules...'
+$IPTABLES -t nat -A POSTROUTING -o $LANIF -j MASQUERADE
+$IPTABLES -A FORWARD -i $LANIF -o $WANIF -m state --state RELATED,ESTABLISHED -j ACCEPT
+$IPTABLES -A FORWARD -i $WANIF -o $LANIF -j ACCEPT
+
+$IPTABLES -t nat -A POSTROUTING -o $WANIF -j MASQUERADE
+$IPTABLES -A FORWARD -i $WANIF -o $LANIF -m state --state RELATED,ESTABLISHED -j ACCEPT
+$IPTABLES -A FORWARD -i $LANIF -o $WANIF -j ACCEPT
+
+echo "starting forward to nuke script"
+
+nohup python forward_to_nuke.py > /dev/null &
+
+echo 'Done.'
